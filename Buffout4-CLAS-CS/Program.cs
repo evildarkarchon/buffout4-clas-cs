6934b42f3621d5e1269a410fee746c394e14f512
Andrew Nelson <evildarkarchon@gmail.com>
2023-02-24 03:26:26 -0800

    Initial project commit

---
+++ Buffout4-CLAS-CS/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Ookii.CommandLine;
+namespace CLAS;
+[ParseOptions(Mode = ParsingMode.LongShort,
+    CaseSensitive = true,
+    ArgumentNameTransform = NameTransform.DashCase,
+    ValueDescriptionTransform = NameTransform.DashCase)]
+class Arguments
+{
+    
+}
+static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
